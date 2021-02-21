5b8d6df5b576a75c5391ec21905771ee06d0a0ad
Clyde Leef <chl51@georgetown.edu>
2021-02-21 12:08:18 -0500

    worked on readme and started thinking about emailing

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # shopping-cart
 Intro to Bus App dev class
 
+
+### Setup:
+To run, set up a virtual environment with conda:
+
+```
+conda create -n shopping-env python=3.8
+conda activate shopping-env
+```
+
+Additionally, you will need to install package dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Finally, create a .env file in your respository, using the file to specify your email address:
+```
+EMAIL_ADDRESS = "example@example.com"
+```
+
+### To Play:
+
+```
+python shopping_cart.py
+```
+Then, follow the prompts and insert your chosen items
--- shopping_cart.py
+++ shopping_cart.py
@@ -36,7 +36,14 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
+import os
 from datetime import datetime
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
 
 go = True
 item = []
@@ -66,6 +73,7 @@ print(" - - - - - - - - - -")
 print("SELECTED PRODUCTS:")
 
 subtotal = 0
+#loop through the numbers and get products and print them
 for items in item:
     id = int(items)
     matching_product = [product for product in products if product['id'] == id]
@@ -85,3 +93,32 @@ print("Come again soon!")
 print(" - - - - - - - - - -")
 
 
+
+
+#emailing stuff:
+#SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+#SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+#
+#client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+#print("CLIENT:", type(client))
+#
+#subject = "Your Receipt from the Green Grocery Store"
+#
+#html_content = "Hello World"
+#print("HTML:", html_content)
+#
+## FYI: we'll need to use our verified SENDER_ADDRESS as the `from_email` param
+## ... but we can customize the `to_emails` param to send to other addresses
+#message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
+#
+#try:
+#    response = client.send(message)
+#
+#    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+#    print(response.status_code) #> 202 indicates SUCCESS
+#    print(response.body)
+#    print(response.headers)
+#
+#except Exception as err:
+#    print(type(err))
+#    print(err)
